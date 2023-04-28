0a62669d03ed0aa455bee19cc9ce36aab4f1a4c9
Mario Gonzales <mariogonzales@Marios-MacBook-Pro.local>
2023-04-28 10:01:16 -0700

    reponsive nav js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
